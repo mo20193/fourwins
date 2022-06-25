978dda44acbc2cb1c697a432f9750b54758298f6
Mohammad El-Masri <masri1993@outlook.de>
2022-06-25 15:25:58 +0200

    CheckAll

--- fourwins/fourwins/Program.cs
+++ fourwins/fourwins/Program.cs
@@ -7,6 +7,7 @@ namespace fourwins
         public static int[,] _fields = new int[6, 7];
         public static Random _random = new Random();
         public const int ZAHL = 5;
+        public static int counter = 1;
 
         public static int roundPlayerOne = 1;
         public static int roundPlayerFive = 1;
@@ -21,10 +22,10 @@ namespace fourwins
 
             do
             {
-                if(PlayingFieldConatinsEmptySpace())
+                if (PlayingFieldConatinsEmptySpace())
                 {
                     var success = false;
-                    
+
                     do
                     {
                         success = GetXYAndCheck(playerOne);
@@ -34,34 +35,37 @@ namespace fourwins
                     } while (!success);
 
                     InsertValue(playerOne);
+
+                    IsWinning(playerOne);
+
                 }
                 else
                 {
                     Console.WriteLine("We have a draw!");
                 }
-                
+
                 ShowField(_fields);
                 Console.ReadKey();
-                
+
                 roundPlayerOne++;
 
-                if (PlayingFieldConatinsEmptySpace())
-                {
-                    do
-                    {
-                        var column = GetColumn();
-                        playerFive.Y = column;
+                //if (PlayingFieldConatinsEmptySpace())
+                //{
+                //    do
+                //    {
+                //        var column = GetColumn();
+                //        playerFive.Y = column;
 
-                        Console.WriteLine("Player Five Turn: " + roundPlayerFive);
+                //        //Console.WriteLine("Player Five Turn: " + roundPlayerFive);
 
-                    } while (!GetXYAndCheck(playerFive));
-                }
-                else
-                {
-                    Console.WriteLine("We have a draw!");
-                }
+                //    } while (!GetXYAndCheck(playerFive));
+                //}
+                //else
+                //{
+                //    Console.WriteLine("We have a draw!");
+                //}
 
-                roundPlayerFive++;
+                //roundPlayerFive++;
 
                 //if(IsWinning(playerOne))
                 //{
@@ -73,15 +77,220 @@ namespace fourwins
             } while (!IsEndOfTheGame());
         }
 
-        //private static bool IsWinning(Player playerOne)
-        //{
+        public static void IsWinning(Player player)
+        {
+            //CheckDown(player);
+            //Console.WriteLine("Down: " + counter);
+            //counter = 1;
 
-        //}
+            CheckRight(player);
+            Console.WriteLine("Right: " + counter);
+            //CheckLeft(player);
+            //Console.WriteLine("Left: " + counter);
+            counter = 1;
+
+            //CheckDiagonalUpRight(player);
+            ////Console.WriteLine("counterDiagonalUPRIGHT: " + counter);
+            //CheckDiagonalDownLeft(player);
+            ////Console.WriteLine("counterDiagonalDownLeft: " + counter);
+            //counter = 1;
+
+            //CheckDiagonalUpLeft(player);
+            ////Console.WriteLine("counterDiagonalUPLeft: " + counter);
+            //CheckDiagonalDownRight(player);
+            ////Console.WriteLine("counterDiagonalDOWNRIGHT: " + counter);
+            //counter = 1;
+        }
+
+        private static void CheckDiagonalUpLeft(Player player)
+        {
+            var x = player.X;
+            var y = player.Y;
+
+            do
+            {
+                x--;
+                y--;
+
+                if (IsFieldInArray(y, x))
+                {
+                    if (_fields[y, x] == player.Value)
+                    {
+                        counter++;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+
+            } while (_fields[y, x] == player.Value);
+        }
+
+        private static void CheckDiagonalDownRight(Player player)
+        {
+            var x = player.X;
+            var y = player.Y;
+
+            do
+            {
+                x++;
+                y++;
+
+                if (IsFieldInArray(y, x))
+                {
+                    if (_fields[y, x] == player.Value)
+                    {
+                        counter++;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+
+            } while (_fields[y, x] == player.Value);
+        }
+
+        private static void CheckDiagonalDownLeft(Player player)
+        {
+
+            var x = player.X;
+            var y = player.Y;
+
+            do
+            {
+                x--;
+                y++;
+
+                if (IsFieldInArray(y, x))
+                {
+                    if (_fields[y, x] == player.Value)
+                    {
+                        counter++;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+
+            } while (_fields[y, x] == player.Value);
+        }
+
+        private static void CheckDiagonalUpRight(Player player)
+        {
+
+            var x = player.X;
+            var y = player.Y;
+
+            do
+            {
+                x++;
+                y--;
+
+                if (IsFieldInArray(y, x))
+                {
+                    if (_fields[y, x] == player.Value)
+                    {
+                        counter++;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+
+            } while (_fields[y, x] == player.Value);
+        }
+
+        private static void CheckLeft(Player player)
+        {
+
+            var x = player.X;
+            var y = player.Y;
+
+            do
+            {
+                x--;
+
+                if (IsFieldInArray(y, x))
+                {
+                    if (_fields[y, x] == player.Value)
+                    {
+                        counter++;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+
+            } while (_fields[y, x] == player.Value);
+        }
+
+        public static void CheckRight(Player player)
+        {
+            var x = player.X;
+            var y = player.Y;
+
+            do
+            {
+                x++;
+                
+                if(IsFieldInArray(y, x))
+                {
+                    if (_fields[y, x] == player.Value)
+                    {
+                        counter++;
+                    }
+                }else
+                {
+                    break;
+                }
+
+            } while (_fields[y, x] == player.Value);
+
+        }
+
+        public static void CheckDown(Player player)
+        {
+
+            var x = player.X;
+            var y = player.Y;
+
+            do
+            {
+                y++;
+
+                if (IsFieldInArray(y, x))
+                {
+                    if (_fields[y, x] == player.Value)
+                    {
+                        counter++;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+
+            } while (_fields[y, x] == player.Value);
+        }
+
+        public static bool IsFieldInArray(int y, int x)
+        {
+            return  x >=0 &&
+                    x <=6 &&
+                    y >=0 &&
+                    y <=5;
+        }
 
         public static void InsertValue(Player player)
         {
             _fields[player.Y, player.X] = player.Value;
         }
+
         public static bool PlayingFieldConatinsEmptySpace()
         {
             for (var i = 0; i < 6; i++)
@@ -89,8 +298,8 @@ namespace fourwins
                 for (int j = 0; j < 7; j++)
                 {
                     var fieldValue = _fields[i, j];
-                    
-                    if(fieldValue == 0)
+
+                    if (fieldValue == 0)
                     {
                         return true;
                     }
@@ -102,7 +311,7 @@ namespace fourwins
 
         private static bool IsEndOfTheGame()
         {
-            if(PlayingFieldConatinsEmptySpace())
+            if (PlayingFieldConatinsEmptySpace())
             {
                 return false;
             }
@@ -113,26 +322,26 @@ namespace fourwins
         private static bool GetXYAndCheck(Player player)
         {
             var column = GetColumn();
-            
-                for (var row = ZAHL; row >= 0; row--)
-                {
-                    if (_fields[row, column] == 0)
-                    {
-                        player.X = column;
-                        player.Y = row;
-                        
-                        return true;
-                    }
-                }
 
-                return false;
+            for (var row = ZAHL; row >= 0; row--)
+            {
+                if (_fields[row, column] == 0)
+                {
+                    player.X = column;
+                    player.Y = row;
+
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         private static int GetColumn()
         {
-           var rnd = _random.Next(0, 7);
+            var rnd = _random.Next(0, 7);
 
-           return rnd;
+            return rnd;
         }
 
         private static void InitializeArray()
@@ -211,10 +420,3 @@ namespace fourwins
         }
     }
 }
-
-//if (_fields[row, column] == 0)
-//{
-//    _fields[row, column] = playerOne.Value;
-
-//    ShowField(_fields);
-//}
